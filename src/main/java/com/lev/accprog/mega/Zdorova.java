7b24bc9d1acc844c642b9ee362401737bbac1026
eksite <denisandipod@mail.ru>
2017-12-01 12:41:53 +0300

    Connet

--- src/main/java/com/lev/accprog/mega/Zdorova.java
+++ src/main/java/com/lev/accprog/mega/Zdorova.java
@@ -1,4 +1,38 @@
 package com.lev.accprog.mega;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
-public class Zdorova {
+class Zdarova
+{
+    public static void main(String[] args) {
+        Zdarova m = new Zdarova();
+        m.testDatabase();
+    }
+
+    private void testDatabase() {
+        try {
+            Class.forName("org.postgresql.Driver");
+            String url = "jdbc:postgresql://localhost:5432/laba";
+            String login = "postgres";
+            String password = "Iamken4o";
+            Connection con = DriverManager.getConnection(url, login, password);
+            try {
+                Statement stmt = con.createStatement();
+                ResultSet rs = stmt.executeQuery("SELECT * FROM food");
+                while (rs.next()) {
+                    String str = rs.getString(2);
+                     System.out.println("taste:" + str);
+                }
+                rs.close();
+                stmt.close();
+            } finally {
+                con.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
+
